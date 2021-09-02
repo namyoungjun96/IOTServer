99570f280b3f55111bf7a035cffc05cc22ab0b2c
namyoungjun96 <ehdgornltls@naver.com>
2021-09-03 05:07:03 +0900

    Camera Raspberry에서 실행해보았으나 실패 원인 아직 불명

--- src/main/java/com/javis/iot/command/ReturnDate.java
+++ src/main/java/com/javis/iot/command/ReturnDate.java
@@ -6,7 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class ReturnDate {
 	public String GetCreateTime(){
 		LocalDateTime Today=LocalDateTime.now();
+		// 년-월-일 시간:분:초로 되있는 정보를 가져온다
+		
 		String createTime=Today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		// 한번 더 포매팅 ( sql datetime에 맞게하기위해 )
 		
 		return createTime;
 	}
