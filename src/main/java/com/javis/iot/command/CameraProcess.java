315e1f1fe909c4e433363da87d9c49e7a6c159b0
namyoungjun96 <ehdgornltls@naver.com>
2021-09-03 21:19:16 +0900

    계속 processbuilder 시도중

--- src/main/java/com/javis/iot/command/CameraProcess.java
+++ src/main/java/com/javis/iot/command/CameraProcess.java
@@ -1,23 +1,31 @@
 package com.javis.iot.command;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CameraProcess { 
 	
-	public void start(String createTime) {
+	public String start(String createTime) {
+		String test;
+		
 		List<String> list = new ArrayList<String>(); 
 		list.add("raspivid -o cam.h264");
 		list.add("MP4Box -add cam.h264 "+createTime+".mp4");
 		
-		ProcessBuilder build = new ProcessBuilder(list); 
+		ProcessBuilder build = new ProcessBuilder(list);
+		build.directory(new File("/home/pi/Desktop/IOTServer")); 
 		
 		try {
 			System.out.println("command: " + build.start());
+			test="성공";
+			return test;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			test="실패";
+			return test;
 		} 
 	} 
 }
--- src/main/java/com/javis/iot/controller/RequestController.java
+++ src/main/java/com/javis/iot/controller/RequestController.java
@@ -2,6 +2,7 @@ package com.javis.iot.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,20 @@ import com.javis.iot.command.*;
 public class RequestController {
 	
 	@GetMapping("/request_behavior")
-	public void request_behavior(HttpServletResponse response) {
+	public String request_behavior(HttpServletResponse response, Model model) {
 		ReturnDate createTime=new ReturnDate();
 		//DownloadBehavior downBehavior = new DownloadBehavior();
 		CameraProcess camera=new CameraProcess();
 		
-		camera.start(createTime.GetCreateTime());
+		String test=camera.start(createTime.GetCreateTime());
 		/*try {
 			downBehavior.Download(createTime.GetCreateTime(), response);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
+		
+		model.addAttribute("test", test);
+		return "test";
 	}
 }
